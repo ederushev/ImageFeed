a4312e42e22ad51a1825934f333be93e7d56a338
ederushev <118970733+ederushev@users.noreply.github.com>
2023-02-16 19:14:10 +0300

    no message

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        print("hello")
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
